7cc5604a24df7b5aa6cf46ee9f54e55ab08ee3ae
yanrongbao1566 <yanrongbao1566@163.com>
2022-08-22 23:50:24 +0800

    feat: 完成首页

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Hot from './components/Hot';
+import Search from './components/Search';
+import { useHttpHook } from '@/hooks';
+import './index.less';
 
 export default function (props) {
   const [state, setState] = useState();
 
+  const [citys, citysLoading] = useHttpHook({
+    url: '/commons/citys',
+  });
+
+  const [houses] = useHttpHook({
+    url: '/house/hot',
+  });
+  // console.log('citys', citys);
   useEffect(() => {}, []);
 
-  return <div>home</div>;
+  return (
+    <div className="home">
+      {/* 登录 */}
+      <Header />
+      {/* 搜索 */}
+      <Search citys={citys} citysLoading={citysLoading} />
+      {/* 民宿 */}
+      <Hot houses={houses} />
+    </div>
+  );
 }
